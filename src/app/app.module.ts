6f566e5874542f785a99df1cb4450d0df865aabc
mtmodesti <marcelomodesti1988@gmail.com>
2022-07-26 08:46:38 -0300

    feat: Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentDataComponent
   ],
   imports: [
     BrowserModule,
